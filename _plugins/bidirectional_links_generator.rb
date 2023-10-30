23032f888aa669e37fc77d923655bf0ff13d4d35
fifkuks <caviskouzz1999@gmail.com>
2023-10-30 19:31:33 +0500

    -

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -20,7 +20,7 @@ class BidirectionalLinksGenerator < Jekyll::Generator
             note_potentially_linked_to.basename,
             File.extname(note_potentially_linked_to.basename)
           )
-        ).gsub('\_', '[ _]').gsub('\-', '[ -]')
+        ).gsub('\_', '[ _]').gsub('\-', '[ -]').capitalize
 
         title_from_data = note_potentially_linked_to.data['title']
         if title_from_data
